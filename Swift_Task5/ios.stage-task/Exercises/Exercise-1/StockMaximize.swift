dc1ef04643e4aad40d2e611ccbbcab45d2179209
brazilec22 <stratienko.artem@gmail.com>
2021-06-14 23:55:19 +0300

    Task 5

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,42 @@
 import Foundation
 
 class StockMaximize {
-
+    
+    var res : Int = -1
+    var buyCount : Int = 0
+    var buySumma : Int = 0
+    
+    var saleCount : Int = 0
+    var saleSumma : Int = 0
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+     
+     
+        let check = prices.allSatisfy({ $0 == prices.first})//)({ $0 == prices.first })
+        
+        print(check)
+        if prices == [] || prices.count == 1 || prices.allSatisfy({ $0 == prices.first}) {
+            self.res = 0
+        }
+        for i in 0...prices.count - 1 {
+            if i != prices.count - 1 {
+            if prices[i] < prices[i+1] {
+                buyCount += 1
+                buySumma += prices[i]
+            } else {
+                saleCount += 1
+                saleSumma += prices[i] * saleCount
+                res += saleSumma - buySumma
+            }
+            } else {
+                saleCount += 1
+                saleSumma += prices[i] * saleCount
+                res += saleSumma - buySumma
+            }
+        }
+        return res
     }
+
 }
+
+
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -1,8 +1,47 @@
 import UIKit
 
 class MessageDecryptor: NSObject {
-    
+    var decodeMessage = ""
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var mutableMessage = message
+        for ch in mutableMessage {
+            if ch.isNumber {
+                let countRepeat = Int(String(ch))
+                mutableMessage.removeFirst()
+                let endIndex = searchMess(message: mutableMessage)
+               
+                let sMess = mutableMessage.index(mutableMessage.startIndex, offsetBy: 1)
+                let eMess = mutableMessage.index(mutableMessage.startIndex, offsetBy: endIndex - 1)
+                //
+                let mMess = mutableMessage.index(mutableMessage.startIndex, offsetBy: endIndex)
+                for _ in 0...countRepeat! - 1 {
+                     decodeMessage += String(mutableMessage[sMess..<eMess])
+                     print("Decode message : \(decodeMessage)")
+                }
+                mutableMessage = String(mutableMessage[mMess..<mutableMessage.endIndex])
+                print("Mut sring : \(mutableMessage)")
+                if !mutableMessage.isEmpty {
+                    print(" call method with \(mutableMessage)")
+                    decryptMessage(mutableMessage)
+                }
+
+            } else {
+                decodeMessage += String(ch)
+                if mutableMessage.count != 0 {
+                    mutableMessage.removeFirst()
+                }
+            }
+        }
+        return decodeMessage
+    }
+    func searchMess(message : String) -> Int {
+        var indexSearch : Int = 0
+        for ch in message {
+            indexSearch += 1
+            if ch == "]" {
+                break
+            }
+        }
+        return indexSearch
     }
 }
